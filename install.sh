ce9e8829cfdf5078ab9d7b402cc12a20c8a68373
Nicola Molinari <nicola.molinari@commercetools.de>
2013-09-01 15:23:44 +0200

    Make install work

--- install.sh
+++ install.sh
@@ -7,28 +7,30 @@ if [ "$OS" == "Darwin" ]
 then
   echo "Detected OS X"
   echo " -> Setting defaults"
-  ./osx
+  ./.osx
 fi
 
 # Create ssh dir if it doesn't exist
-if [ ! -f '~/.ssh/' ];
+if [ -f '~/.ssh/' ];
 then
   mkdir '~/.ssh/'
   echo "Creating dir ~/.ssh"
 fi
 
 # Create .gitconfig.local if doesn't exist
-if [ ! -f '~/.gitconfig.local' ];
+if [ -f '~/.gitconfig.local' ];
 then
   echo "Creating template for '.gitconfig.local'"
   cp .gitconfig.local ~/.gitconfig.local
 fi
 
 #create the symlinks for all dotfiles
-declare -a dotfiles=(.aliases .colors .gitconfig .profile .vimrc .vim, .iterm2 .ssh/config)
+declare -a dotfiles=(.aliases .colors .gitconfig .profile .vimrc .vim .iterm2 .ssh/config)
 for i in "${dotfiles[@]}"
 do
   :
+  echo "Removing symlink $i"
+  rm -rf ~/$i
   echo "Symlinking $i"
   ln -sf `pwd`/$i ~/$i
 done
@@ -39,4 +41,5 @@ done
 
 echo "Symlinking .sbt/plugins"
 mkdir -p ~/.sbt
+rm -rf ~/.sbt/plugins
 ln -sf `pwd`/.sbt/plugins ~/.sbt/plugins
